26827e13246a4120fb5912ff428e3036b0fa5457
Chris Hawkes <chawkesmexico@gmail.com>
2018-02-24 23:23:17 +0000

    [CH][MS] Travis integration.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'bpk-stylesheets';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
